24876e52927072d5ba83280fce5f4d4ec7189c36
Rakshith99B <rakshithbanari99@gmail.com>
2026-02-06 11:50:49 +0530

    Internship Activities

---
+++ src/Day5/day5_fundamentals.py
@@ -0,0 +1,26 @@
+print("First Function Program\n")
+def greet():
+	print("Hello! Welcome to Python.")
+greet()
+print("\n")
+
+############# Addition #############
+print("Addition Example:")
+def add(a, b):
+    return a + b
+result = add(5, 3)
+print(result)
+print("\n")
+
+############# Variable Scope #############
+print("Variable Scope Example:")
+x=10
+def show_scope():
+    x=5
+    print(x)
+show_scope()
+print(x)
+
+
+
+
