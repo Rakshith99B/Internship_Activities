183c4d07f171b90f66840e9cf60e74c6d1369930
Rakshith99B <rakshithbanari99@gmail.com>
2026-02-11 14:58:16 +0530

    Internship Activies

--- src/Day9/day9_fundamentals.py
+++ src/Day9/day9_fundamentals.py
@@ -1,5 +1,5 @@
 ############ Pandas ############
-print("Pandas ")
+print("Pandas: ")
 import pandas as pd
 
 s1 = pd.Series([10, 20, 30, 40])
@@ -9,14 +9,14 @@ print(s1)
 print(s2)
 
 ############ Indexing and selection ############
-print("\nIndexing and selection ")
+print("\nIndexing and selectio: ")
 marks = pd.Series([85, 90, 78], index=['Math', 'Physics', 'Chemistry'])
 
 print(marks['Math'])
 print(marks[['Math', 'Chemistry']])
 
 ############ Boolean Masking Series ############
-print("\nBoolean Masking Series ")
+print("\nBoolean Masking Series: ")
 scores = pd.Series([45, 67, 89, 34, 90])
 
 passed = scores[scores > 60]
@@ -24,14 +24,14 @@ print(passed)
 
 ############# Handling Missing Values #############
 
-print("\nHandling Missing Values ")
+print("\nHandling Missing Values: ")
 data = pd.Series([10, None, 30, None])
 
 print(data.isnull())
 print(data.fillna(0))
 
 ############## Vectorized String ###############
-print("\nVectorized String ") 
+print("\nVectorized String: ") 
 
 names = pd.Series(['Alice', 'bob', 'CHARLIE'])
 
