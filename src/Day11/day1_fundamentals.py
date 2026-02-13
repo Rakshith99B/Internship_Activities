80458e968f8209006e1cfe0ba5a7b1e5c9ff4110
Rakshith99B <rakshithbanari99@gmail.com>
2026-02-13 11:53:19 +0530

    uploaded

---
+++ src/Day11/day1_fundamentals.py
@@ -0,0 +1,34 @@
+######## Plot ########
+
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 11]
+
+plt.plot(x, y)
+plt.show()
+
+######## Scatter Plot ########
+
+x1 = [1, 2, 3, 4, 5]
+y1 = [5, 4, 3, 2, 1]
+
+plt.scatter(x1, y1)
+plt.show()
+
+######## Bar Chart ########
+
+categories = ['A', 'B', 'C']
+values = [11, 20, 15]
+
+plt.bar(categories, values)
+plt.show()
+
+######## Customizing Plots ########
+
+plt.subplot(1, 2, 1)
+plt.plot([1,2,3], [1,4,9])
+plt.title("Line Plot")
+plt.subplot(1, 2, 2)
+plt.bar(['A','B','C'], [3,7,5])
+plt.title("Bar Chart")
+plt.show()
