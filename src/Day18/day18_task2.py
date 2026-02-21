22e8b35c68f681284b5e6853ae4a3b0ee2af282a
Rakshith99B <rakshithbanari99@gmail.com>
2026-02-21 21:17:23 +0530

    Internship Activities

---
+++ src/Day18/day18_task2.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect(r"C:\Users\raksh\sqlite\sql_internship.db")
+
+query = """
+SELECT
+    interns.name,
+    interns.track,
+    mentors.mentor_name
+FROM interns
+INNER JOIN mentors
+ON interns.track = mentors.track
+"""
+
+df = pd.read_sql_query(query, conn)
+
+print(df)
+
+conn.close()
